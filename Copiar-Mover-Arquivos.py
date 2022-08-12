a8da16631f8dad3353242b7e852ab3ca1363589a
Dennis Porto <60332417+dennisreis-dev@users.noreply.github.com>
2022-08-12 14:21:16 -0300

    py

---
+++ Copiar-Mover-Arquivos.py
@@ -0,0 +1,26 @@
+# listar arquivos
+import os
+import shutil
+
+#arquivos = os.listdir()
+#print(arquivos)
+#
+# renomear
+## os.rename('Vendas - 1.xlsx', 'Vendas 1.xlsx')
+#
+# mover
+##os.rename('Vendas - 1.xlsx', 'Vendas\Vendas - 1.xlsx')
+#
+# copiar arquivos
+#shutil.copy2('Vendas - 1.xlsx', 'Vendas\Vendas - 1.xlsx')
+
+lista_arquivos = os.listdir()
+
+for arquivo in lista_arquivos:
+    if 'xlsx' in arquivo:
+        if "Compras" in arquivo:
+            # jogar pra pasta de compras
+            os.rename(arquivo, f'Compras//{arquivo}')
+        elif "Vendas" in arquivo:
+            # jogar pra pasta de vendas
+            os.rename(arquivo, f'Vendas//{arquivo}')
